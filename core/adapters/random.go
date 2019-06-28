4737f050c8c152feeaf5e73350c851aaa651efa9
Steve Ellis <email@steveell.is>
2019-06-27 19:21:57 -0700

    add warning comment to RNG adapter documentation

--- core/adapters/random.go
+++ core/adapters/random.go
@@ -8,7 +8,12 @@ import (
 	"github.com/smartcontractkit/chainlink/core/store/models"
 )
 
-// Random adapter type holds no fields
+// Random adapter generates a number between 0 and 2**256-1.
+//
+// WARNING: The random number adapter as implemented is not verifiable.
+// We intend to either improve it in the future, or introduce a verifiable alternative.
+// For now it is provided as an alternative to making web requests for random numbers,
+// which is similarly unverifiable and has additional possible points of failure.
 type Random struct{}
 
 // Perform returns a random uint256 number in 0 | 2**256-1 range
